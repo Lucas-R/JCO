eac7ce63ab576b6f8529238b72265abb45491d70
Lucas Rodrigues <lucas.rodrigues@mktnow.com.br>
2024-08-02 09:27:44 -0300

    header init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./style";
+
+export function Header() {
+    return (
+        <S.WrapperHeader>
+            <S.ContainerHeader>
+                <h1> Prazer de morar nos Jardins </h1>
+            </S.ContainerHeader>
+        </S.WrapperHeader>
+    )
+}
---
+++ src/components/Header/style.ts
@@ -0,0 +1,5 @@
+import { styled } from 'styled-components'
+
+export const WrapperHeader = styled.header``;
+
+export const ContainerHeader = styled.div``;
